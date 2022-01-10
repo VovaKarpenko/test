ffee25c79c48a470184af6b4b872e6d46e4f8295
vovakarpenko <vovakarpenko781@gmail.com>
2022-01-11 01:17:38 +0200

    Pyramid

--- src/test.java
+++ src/test.java
@@ -1,19 +1,50 @@
+import com.apple.eawt.AppEventListener;
+
 import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
-        int i = 5;
-makeTower(i);
-    }
 
-    public static void makeTower(int disk) {
-        int[][] array = new int[disk][3];
-        for (int i = 0; i < disk; i++) {
-            array[i][0] = i+1;
+        int[][] array ={{1,0,0}
+                       ,{2,0,0}
+                       ,{3,0,0}}
+                ;
+        String[][] arrayStr= new String[array[array.length-1][0]][3];
+
+        for (int i = array[array.length-1][0]; i > 1; i--) {
+            for (int j = 0; j < 3; j++) {
+
+            }
         }
-        Arrays.stream(array).map(Arrays::toString).forEach(System.out::println);
+
 
     }
-}
+
+    public static String[][] swapBeautiful(String[][] tower, int[] heightTo, int[] heightFrom, int from, int to) {
+        String  swap = tower[heightFrom[from]][from];
+        tower[heightFrom[from]][from] = tower[heightTo[to]][to];
+        tower[heightTo[to]][to] = swap;
+        return tower;
+    }
 
 
+}/*
+
+                           |
+                           |
+                           |
+                           |
+                           |
+                           |
+
+     |
+     |     (*)
+     |    (***)
+     |   (*****)
+     |  (*******)
+     | (*********)
+     |(***********)
+
+
+
+   */
